614820ba67f19caf6b8a1afe5635cc52ee2ddad5
Mamady Sillah <supurpisces310@msn.com>
2020-09-19 18:02:06 -0400

    installed mongoose dependencies. Imported MongooseModule in app.module.ts and then connected mongodb to localhose/quotes. Added useNewUrlParser for added measure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuotesController } from './quotes/quotes.controller';
 import { QuotesService } from './quotes/quotes.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/quotes')],
   controllers: [AppController, QuotesController],
   providers: [AppService, QuotesService],
 })
