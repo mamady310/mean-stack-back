cf262dd024045c621206ab8c8893ac19eed1c87a
Mamady Sillah <supurpisces310@msn.com>
2020-09-18 20:36:44 -0400

    added getQuote method in quotes.controller to return the id when /(insert numerical) value is put into browser url

--- src/quotes/quotes.controller.ts
+++ src/quotes/quotes.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateQuoteDto } from './dto/create-quote.dto';
 import { QuotesService } from './quotes.service';
 import { ApiTags }from '@nestjs/swagger';
@@ -17,6 +17,12 @@ constructor(private quotesService: QuotesService) {
     getQuotes(): string {
         return this.quotesService.getQuotes();
     }
+    @Get(':id') //param will contain all dynamic ids 
+    
+    getQuote(@Param() param) {
+      return `${param.id}`;  
+    }
+
     @Post()
 
    createQuote(@Body() createQuoteDto: CreateQuoteDto): any {
