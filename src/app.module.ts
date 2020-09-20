86e7a6f53f0b5791c67b9020215032daa4a0a0df
Mamady Sillah <supurpisces310@msn.com>
2020-09-19 20:42:10 -0400

    created a schema for database in quotes.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuotesController } from './quotes/quotes.controller';
-import { QuotesService } from './quotes/quotes.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QuotesModule } from './quotes/quotes.module';
 
