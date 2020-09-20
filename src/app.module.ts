45afdb2548644bca99adacfd86520e14f3e11944
Mamady Sillah <supurpisces310@msn.com>
2020-09-19 20:38:17 -0400

    created quotes module. deleted quotes service and quotes controller from app.modules.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { QuotesController } from './quotes/quotes.controller';
 import { QuotesService } from './quotes/quotes.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { QuotesModule } from './quotes/quotes.module';
 
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URI, {
     useNewUrlParser: true,
-  })],
-  controllers: [AppController, QuotesController],
-  providers: [AppService, QuotesService],
+  }), QuotesModule],
+  controllers: [AppController, ],
+  providers: [AppService, ],
 })
 export class AppModule {}
